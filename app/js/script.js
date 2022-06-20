92e4526a9faaf560e6044e59b7519742421945cb
21nathanmutombo <nathanmtb@gmail.com>
2022-06-20 16:37:30 -0500

    v1

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click Hamburger');
+
+    if(header.classList.contains('open')){  // Close Hamburger 
+       header.classList.remove('open');
+       overlay.classList.remove('fade-in');
+       overlay.classList.add('fade-out');
+    }
+    else {                                 // Open Hamburger menu
+       header.classList.add('open');
+       overlay.classList.remove('fade-out');
+       overlay.classList.add('fade-in');
+    }
+});
